ee03a184f349b0fcdc2466c7e0be91ddd76d6bb0
merarichardson-create <merarichardson@gmail.com>
2026-02-16 22:14:43 +0000

    Add basic Streamlit app

---
+++ streamlit_app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(page_title="Airbnb Cohost Research", layout="wide")
+
+st.title("Airbnb Cohost Research")
+st.write("Welcome to the Airbnb Cohost Research application")
+
+st.markdown("""
+This is a research tool for analyzing Airbnb cohost data and trends.
+""")
